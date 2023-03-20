ca25441a75fbfcf226322b03ba674e3e1f57c3d1
Akash Ude <88569354+akashude@users.noreply.github.com>
2023-03-20 14:42:15 +0530

    Create functs.groovy

---
+++ vars/functs.groovy
@@ -0,0 +1,24 @@
+def newDownload(repo)
+{
+  git 'https://github.com/akashude/${repo}.git'
+{
+def newBuild()
+{
+  sh 'mvn package'
+}
+
+def newDeployment(job,IP,appname)
+{
+  sh 'scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@{IP}:/var/lib/tomcat9/webapps/{appname}.war'
+}
+
+def newTesting(job,testfile)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${job}/${testfile}'
+}
+
+def newDelivery(submitter,job,IP,appname)
+{
+  input message: 'Aprove this for Delivery', submitter: ${submitter}
+  newDeployment(job,IP,appname)
+}
