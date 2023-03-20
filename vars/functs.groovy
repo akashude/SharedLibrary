01551acf14fd8caf484420ef18f2b2abd6037c8d
Akash Ude <88569354+akashude@users.noreply.github.com>
2023-03-20 15:00:58 +0530

    Update functs.groovy

--- vars/functs.groovy
+++ vars/functs.groovy
@@ -9,12 +9,12 @@ def newBuild()
 
 def newDeployment(job,IP,appname)
 {
-  sh 'scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat9/webapps/${appname}.war'
+  sh "scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat9/webapps/${appname}.war"
 }
 
 def newTesting(job,testfile)
 {
-  sh 'java -jar /var/lib/jenkins/workspace/${job}/${testfile}'
+  sh "java -jar /var/lib/jenkins/workspace/${job}/${testfile}"
 }
 
 def newDelivery(submitter,job,IP,appname)
